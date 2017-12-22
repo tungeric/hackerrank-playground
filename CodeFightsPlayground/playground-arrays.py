850c282a58cac3047b67aaba5091b629f0456957
Eric Tung <eric.p.tung@gmail.com>
2017-12-22 13:36:04 -0800

    complete not repeating characters in arrays

--- CodeFightsPlayground/playground-arrays.py
+++ CodeFightsPlayground/playground-arrays.py
@@ -13,3 +13,25 @@ def firstDuplicate(a):
         idx += 1
     return duplicate
 
+# FIRST ONE WITHOUT DUPLICATES
+
+def firstNotRepeatingCharacter(s):
+    counter = {}
+    idx = 0
+    bucket = []
+    while idx < len(s):
+        if s[idx] in counter:
+            counter[s[idx]] += 1
+            if s[idx] in bucket:
+                bucket.remove(s[idx])
+        else:
+            counter[s[idx]] = 1
+            bucket.append(s[idx])
+        idx += 1
+    if len(bucket) == 0:
+        return '_'
+    return bucket[0]
+    # for key, value in counter.items():
+    #     if value == 1:
+    #         char = key
+    #         break
