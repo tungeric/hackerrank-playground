29ce1e6874ece3c4834132b77aac141be38ab414
Eric Tung <eric.p.tung@gmail.com>
2017-12-22 17:09:02 -0800

    complete christmas toys challenge

--- CodeFightsPlayground/playground-arrays.py
+++ CodeFightsPlayground/playground-arrays.py
@@ -32,6 +32,92 @@ def firstNotRepeatingCharacter(s):
         return '_'
     return bucket[0]
 
+# ROTATE IMAGE
+
+def rotateImage(a):
+    w = len(a)
+    rotatedImage = [[0 for x in range(w)] for y in range(w)]
+    for x in range(0,w):
+        for y in range(0,w):
+            rotatedImage[y][w-x-1] = a[x][y]
+    return rotatedImage
+
+# CRYPT
+
+def isCryptSolution(crypt, solution):
+    solutionHash = createSolutionHash(solution)
+    numWordArray = []
+    for word in crypt:
+        numWordArray.append(convertToNum(word, solutionHash))
+    
+    # check if anything starts with 0
+    for val in numWordArray:
+        if len(val) > 1 and val[0] == '0':
+            return False
+    
+    # check if numbers add up
+    numArray = []
+    for numWord in numWordArray:
+        numArray.append(int(numWord))
+    if not numArray[0] + numArray[1] == numArray[2]:
+         return False
+    return True
+    
+def createSolutionHash(solution):
+    solutionHash = {}
+    for subArray in solution:
+        solutionHash[subArray[0]] = subArray[1]
+    return solutionHash
+    
+def convertToNum(str, solutionHash):
+    numCharArray = []
+    for char in str:
+        numCharArray.append(getNumForChar(char, solutionHash))
+    return "".join(numCharArray)
+
+def getNumForChar(char, solution):
+    return solution[char]
+
+
+# CHRISTMAS TOYS
+# input: toys: [[6,2,4], 
+            #  [], 
+            #  [], 
+            #  [], 
+            #  [5,1], 
+            #  [], 
+            #  [3]]
+# output: [[0], 
+            #  [6,2,4], 
+            #  [3,5,1]]
+
+def christmasToys(toys):
+    topNode = findTopNode(toys)
+    result = []
+    currLevel = [topNode]
+    while currLevel:
+        result.append(currLevel)
+        nextLevel = []
+        for node in currLevel:
+            for nextNode in toys[node]:
+                nextLevel.append(nextNode)
+        currLevel = nextLevel
+    return result
+    
+def findTopNode(toys):
+    root = -1
+    flattenedToys = []
+    for i in toys:
+        for j in i:
+            flattenedToys.append(j)
+    numToys = len(toys)
+    for x in range(0, numToys):
+        if not x in flattenedToys:
+            root = x
+            break
+    return root
+
+
 # HELPFUL THINGS:
 # interate through dictionary
     # for key, value in counter.items():
