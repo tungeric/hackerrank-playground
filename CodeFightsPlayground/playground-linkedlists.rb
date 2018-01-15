da5fef10a8e1e5145dc486e904d9aa9f447f96f0
Eric Tung <eric.p.tung@gmail.com>
2018-01-14 20:59:58 -0800

    Add two huge numbers linked lists

--- CodeFightsPlayground/playground-linkedlists.rb
+++ CodeFightsPlayground/playground-linkedlists.rb
@@ -34,3 +34,35 @@ def isListPalindrome(l)
     end
     return array == array.reverse
 end
+
+# Add two huge numbers
+# Input: a = [9876, 5432, 1999]
+#        b = [1, 8001]
+# Output: [9876, 5434, 0]
+# Input: a = [123, 4, 5]
+#        b = [100, 100, 100]
+# Output: [223, 104, 105]
+
+def addTwoHugeNumbers(a, b)
+    a_num = ""
+    b_num = ""
+    while a
+        a_num += '%04d' % a.value
+        a = a.next
+    end
+    puts a_num
+    while b
+        b_num += '%04d' % b.value
+        b = b.next
+    end
+    puts b_num
+    total = a_num.to_i + b_num.to_i
+    result = []
+    return [0] if total == 0
+    while total > 0
+        result.unshift(total%10000)
+        total /= 10000
+    end
+    result
+end
+
